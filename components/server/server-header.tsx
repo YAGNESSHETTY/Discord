2d5bdcb302a9fb00abf19131d0d33f6f2068652b
YAGNESSHETTY <80250245+YAGNESSHETTY@users.noreply.github.com>
2025-01-20 10:09:06 +0530

    Fixed Manage Members functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -31,13 +31,13 @@ const ServerHeader = ({server , role} : ServerHeaderProps) => {
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={()=> onOpen("editServer" , {server})} className="px-3 py-2 text-sm cursor-pointer">
                         Server Settings 
                         <Settings className="w-4 h-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={()=> onOpen("members" , {server })} className="px-3 py-2 text-sm cursor-pointer">
                         Manage Members
                         <Users className="w-4 h-4 ml-auto"/>
                     </DropdownMenuItem>
