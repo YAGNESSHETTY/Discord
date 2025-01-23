f99dd5ff2cd42e3358643dd24499a4ee9a752f8a
Yagnesh Shetty <yagnesh.shetty@hbwsl.com>
2025-01-23 18:43:20 +0530

    Added socket-io and made changes

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,21 +1,20 @@
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
-import ServerSidebar from "@/components/server/server-sidebar";
+import {ServerSidebar} from "@/components/server/server-sidebar";
 
 const ServerIdLayout = async (
     { children , params } :
     { children : React.ReactNode ; params : {serverId : string } ;} 
 ) => {
     const profile = await currentProfile() ;
-    const {serverId} = await params ;
     if(!profile){
         return redirect('/sign-in') ;
     }
 
     const server = await db.server.findUnique({
         where : {
-            id: serverId ,
+            id: params.serverId ,
             members : {
                 some : {
                     profileId : profile.id ,
@@ -31,7 +30,7 @@ const ServerIdLayout = async (
     return ( 
         <div className="h-full">
             <div className="fixed invisible md:visible md:flex h-full w-60 z-20 flex-col inset-y-0">
-                <ServerSidebar serverId= {serverId}/>
+                <ServerSidebar serverId= {params.serverId}/>
             </div>
 
             <main className="h-full md:pl-60">
